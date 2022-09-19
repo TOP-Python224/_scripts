071a4b885b42ef49718863aa9a9ba11c499af6ec
Gennadiy <renardes@gmail.com>
2022-09-19 22:21:07 +0500

    чтение и запись в файл

--- open_function.py
+++ open_function.py
@@ -4,3 +4,12 @@ script_dir = path[0]
 hw_path = '\\'.join((script_dir, 'HW.txt'))
 
 f_in = open(hw_path, encoding='utf-8')
+hw_text = f_in.read()
+f_in.close()
+
+print(hw_text)
+
+
+with open(hw_path, 'w', encoding='utf-8') as f_out:
+    f_out.write('Тестовая запись')
+
