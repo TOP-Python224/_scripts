5bc23f9ace62f82044448ce0d5bb74e448ba47df
Gennadiy <renardes@gmail.com>
2022-09-21 22:42:24 +0500

    работа с объектами разных модулей
    в различных пространствах имён

--- module3.py
+++ module3.py
@@ -1,13 +1,41 @@
 """Третий модуль программы демонстратора работы с модулями."""
+
 print('начало выполнения модуля 3')
 
+# импорт объектов осуществляется по ссылке
 from module2 import M1_VAR, M2_VAR
 
-from pprint import pprint
+import module2
 
 
 M3_VAR = 300
 
-print(M1_VAR + M2_VAR + M3_VAR)
+print(f'\n{M1_VAR = }\n{M2_VAR = }\n{M3_VAR = }')
+
+# объект в пространстве имён модуля 2 перезаписывается, и теперь имена module2.M2_VAR и M2_VAR (в прострастве имён модуля 3) ссылаются на разные объекты
+module2.M2_VAR = 250
+print(f'\n{module2.M2_VAR = }\n{M2_VAR = }')
+
+
+module2.new_obj = []
+module2.new_obj.append(222)
+
+# импорт объекта осуществляется по ссылке
+from module2 import new_obj
+
+# изменяемый объект
+print(f'\n{module2.new_obj = }\n{new_obj = }')
+
+# объект изменяется, но не перезаписывается
+module2.new_obj.append(233)
+# ссылочная зависимость сохраняется
+print(f'\n{module2.new_obj = }\n{new_obj = }')
+
+
+# объект перезаписывается
+module2.new_obj = [222, 233, 244]
+# ссылочная зависимость разрывается
+print(f'\n{module2.new_obj = }\n{new_obj = }\n')
+
 
 print('конец выполнения модуля 3')
