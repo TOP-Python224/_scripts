7d96925d713739d0f2e25a0d320e5fe40136fed6
Gennadiy <renardes@gmail.com>
2022-11-02 23:25:16 +0500

    видимость атрибутов: публичные, частные, защищённые
    пример инкапсуляции

---
+++ oop/attributes_organiztion.py
@@ -0,0 +1,24 @@
+from datetime import datetime as dt
+
+
+class Organization:
+    def __init__(self, name: str, inn: str):
+        self.name = name
+        self.inn = inn
+        self._budget = 0
+        self.__budget_calendar = {}
+
+    def __set_budget(self, amount: int):
+        if dt.now().year not in self._budget:
+            self.__budget_calendar[dt.now().year] = amount
+            self._budget = amount
+
+
+org = Organization('Рога-и-Копыта', '123456789')
+print(org.__dict__)
+
+
+org.budget = 100000
+org.budget = 'денег-нет-шлите-телеграммы'
+
+print(org.__dict__)
