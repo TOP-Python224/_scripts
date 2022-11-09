a1e191174cd6b55f7c39d44a9c621b466279c349
Gennadiy <renardes@gmail.com>
2022-11-09 21:34:06 +0500

    перегрузка оператора -
    __sub__() и __rsub__()

--- oop/methods_point.py
+++ oop/methods_point.py
@@ -5,7 +5,7 @@ Coordinates = tuple[Real, Real]
 
 class Point:
     """Описывает точку на плоскости в декартовой системе координат."""
-    def __init__(self, x: float, y: float):
+    def __init__(self, x: Real, y: Real):
         self.x = x
         self.y = y
 
@@ -23,11 +23,21 @@ class Point:
             raise TypeError
 
     # p1 - axes_zero
-    # p1.__sub_(axes_zero)
-    def __sub__(point1, point2):
+    # p1.__sub__(axes_zero)
+    def __sub__(point, other):
         """Вычисляет и возвращает расстояние между двумя точками."""
-        if isinstance(point2, Point):
-            return ((point1.x - point2.x)**2 + (point1.y - point2.y)**2)**0.5
+        if isinstance(other, Point):
+            return ((point.x - other.x)**2 + (point.y - other.y)**2)**0.5
+        elif isinstance(other, Real):
+            return point - Point(other, other)
+        else:
+            raise TypeError
+
+    # 5 - p2
+    # p2.__rsub__(5)
+    def __rsub__(point, other):
+        if isinstance(other, Real):
+            return point - Point(other, other)
         else:
             raise TypeError
 
@@ -42,3 +52,5 @@ print(p1 < (5, 5))
 print(p2 < axes_zero)
 
 print(p1 - axes_zero)
+print(p1 - 12)
+print(5 - p2)
