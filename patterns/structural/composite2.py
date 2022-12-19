075da1c49d8f289d7324d73118653f1f90aa126c
Gennadiy <renardes@gmail.com>
2022-12-19 21:55:10 +0500

    Компоновщик: слой, подключающийся к другому слою

--- patterns/structural/composite2.py
+++ patterns/structural/composite2.py
@@ -13,15 +13,42 @@ class Neuron:
         return self.name
 
 
+class NeuronLayer(list):
+    def __init__(self, name: str, count: int = 2):
+        super().__init__()
+        self.name = name
+        for i in range(1, count+1):
+            self.append(Neuron(f'нейрон {i} слоя {self.name}'))
+
+    def connect_to(self, other: 'NeuronLayer'):
+        for neuron_out in self:
+            for neuron_in in other:
+                neuron_out.connect_to(neuron_in)
+
+    def __repr__(self):
+        return '\n'.join(repr(n) for n in self)
+
+
+
 n1 = Neuron('нейрон 1')
 n2 = Neuron('нейрон 2')
 n2.connect_to(n1)
 
-print(n1.name)
-print(n1.inputs)
-print(n1.outputs, end='\n\n')
+nl1 = NeuronLayer('1', 3)
+nl2 = NeuronLayer('2', 2)
+print(nl1, nl2, sep='\n\n', end='\n\n')
 
-print(n2.name)
-print(n2.inputs)
-print(n2.outputs, end='\n\n')
+nl2.connect_to(nl1)
 
+for n in nl1:
+    print(n.name)
+    print('\tвходы:', n.inputs)
+    print('\tвыходы:', n.outputs)
+print()
+
+for n in nl2:
+    print(n.name)
+    print('\tвходы:', n.inputs)
+    print('\tвыходы:', n.outputs)
+
+nl1.connect_to(n1)
