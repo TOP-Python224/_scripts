397fac2099c638dfac138b20e0d718f6c52a8c8c
Gennadiy <renardes@gmail.com>
2022-12-19 22:22:46 +0500

    Компоновщик: подключение слоя к другому слою или нейрону

--- patterns/structural/composite2.py
+++ patterns/structural/composite2.py
@@ -1,3 +1,8 @@
+from typing import Union
+
+
+Neurons = Union['Neuron', 'NeuronLayer']
+
 
 class Neuron:
     def __init__(self, name: str):
@@ -8,6 +13,9 @@ class Neuron:
     def connect_to(self, other: 'Neuron'):
         self.outputs += [other]
         other.inputs += [self]
+
+    def __iter__(self):
+        yield self
 
     def __repr__(self):
         return self.name
@@ -20,7 +28,7 @@ class NeuronLayer(list):
         for i in range(1, count+1):
             self.append(Neuron(f'нейрон {i} слоя {self.name}'))
 
-    def connect_to(self, other: 'NeuronLayer'):
+    def connect_to(self, other: Neurons):
         for neuron_out in self:
             for neuron_in in other:
                 neuron_out.connect_to(neuron_in)
@@ -39,6 +47,7 @@ nl2 = NeuronLayer('2', 2)
 print(nl1, nl2, sep='\n\n', end='\n\n')
 
 nl2.connect_to(nl1)
+nl1.connect_to(n1)
 
 for n in nl1:
     print(n.name)
@@ -51,4 +60,3 @@ for n in nl2:
     print('\tвходы:', n.inputs)
     print('\tвыходы:', n.outputs)
 
-nl1.connect_to(n1)
