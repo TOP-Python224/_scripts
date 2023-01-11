29e93acc9d1a8eebc309f18276ce1b59fac7ff9e
Gennadiy <renardes@gmail.com>
2023-01-11 21:12:26 +0500

    MVC: архитектура и каркас кода

---
+++ mvc/2/main.py
@@ -0,0 +1,25 @@
+"""Приложение Сапёр — с использованием MVC."""
+
+
+class Cell:
+    pass
+
+
+class Model:
+    pass
+
+
+class View:
+    pass
+
+
+class Controller:
+    pass
+
+
+def main():
+    """Точка входа."""
+
+
+if __name__ == '__main__':
+    main()
