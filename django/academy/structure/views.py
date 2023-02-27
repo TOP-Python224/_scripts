37e6c36e77f0a94b90c8e9869dbd0e39f0cd36b6
Gennadiy <renardes@gmail.com>
2023-02-27 23:55:26 +0500

    динамическое формирование меню страниц

--- django/academy/structure/views.py
+++ django/academy/structure/views.py
@@ -3,6 +3,23 @@ from django.views.generic import ListView, DetailView
 from structure.models import Faculty
 
 
+faculty_menu = {
+    faculty.acronym: [
+        {'name': 'Head', 'url': f'/{faculty.acronym}/head/'},
+        {'name': 'Departments', 'url': f'/{faculty.acronym}/departments/'},
+        {'name': 'Science', 'url': f'/{faculty.acronym}/science/'},
+    ]
+    for faculty in Faculty.objects.all()
+}
+
+department_menu = (
+    {'name': 'Head', 'url': 'head/'},
+    {'name': 'Teachers', 'url': 'teachers/'},
+    {'name': 'Groups', 'url': 'groups/'},
+    {'name': 'Programs', 'url': 'programs/'},
+)
+
+
 class IndexView(ListView):
     model = Faculty
     template_name = 'structure/index.html'
@@ -14,6 +31,7 @@ class FacultyView(DetailView):
 
     def get_context_data(self, **kwargs):
         return {
-            'page_title': self.object.acronym.upper()
+            'page_title': self.object.acronym.upper(),
+            'top_menu': faculty_menu[self.object.acronym],
         } | super().get_context_data(**kwargs)
 
