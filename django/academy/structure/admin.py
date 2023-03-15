910098ac86ead06d95ba5ec89e1102ad40650d7b
Gennadiy <renardes@gmail.com>
2023-03-15 23:46:27 +0500

    подключение пользовательских моделей к встроенному приложению admin

--- django/academy/structure/admin.py
+++ django/academy/structure/admin.py
@@ -1,3 +1,56 @@
+from django import forms
 from django.contrib import admin
 
-# Register your models here.
+from structure import models
+
+
+# admin.site.register(models.Department)
+# class DepartmentInline(admin.TabularInline):
+#     model = models.Department
+
+
+class DepartmentForm(forms.ModelForm):
+    groups = forms.ModelMultipleChoiceField(
+        queryset=models.Group.objects.all()
+    )
+
+    class Meta:
+        model = models.Department
+        fields = ('name', 'building', 'financing', 'faculty', 'groups')
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['groups'].initial =
+
+
+@admin.register(models.Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    form = DepartmentForm
+
+    def get_changeform_initial_data(self, request):
+        initials = super().get_changeform_initial_data(request)
+        initials['groups'] = [o for o in models.Group.objects.filter(department__name=initials['name'])]
+        return initials
+
+
+admin.site.register(models.Faculty)
+# @admin.register(models.Faculty)
+# class FacultyAdmin(admin.ModelAdmin):
+#     inlines = [
+#         DepartmentInline
+#     ]
+
+
+admin.site.register(models.Subject)
+admin.site.register(models.Teacher)
+admin.site.register(models.Curator)
+admin.site.register(models.Student)
+admin.site.register(models.Lecture)
+
+
+@admin.register(models.Group)
+class GroupAdmin(admin.ModelAdmin):
+    fields = ('name', 'year', 'department', 'students', 'curators')
+    list_display = ('year', 'name')
+    list_display_links = ('name',)
+
--- django/academy/structure/apps.py
+++ django/academy/structure/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class StructureConfig(AppConfig):
     default_auto_field = 'django.db.models.AutoField'
     name = 'structure'
+    verbose_name = 'University Structure'
